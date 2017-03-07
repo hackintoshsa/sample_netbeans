703783250f56842285ee8c1c24ce3ab3a06d2a77
HackintoshSA <hackintoshsa@gmail.com>
2017-03-07 10:37:36 +0200

    A Sample Hello World

---
+++ src/helloworld.py
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+        #A Simple Hello World Program
+if __name__ == "__main__":
+    #Print Hello World
+    print "Hi World"
